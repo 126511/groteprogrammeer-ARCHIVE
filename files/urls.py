d35834c71337a154a396c7411e14fc2abbb08318
126511 <126511@dgwcloud.nl>
2021-02-26 21:23:12 +0100

    removed //

--- files/urls.py
+++ files/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<slug:chapterpath>/<slug:path>/", views.file_view, name="fileview"),
-    path("/latestpage", views.latestpage, name="latestpage")
+    path("latestpage", views.latestpage, name="latestpage")
 ]
